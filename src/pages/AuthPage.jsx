4b80833bf14d10d489518f2a6fa8be8cfe3a7727
Alsa <leonidremix@gmail.com>
2025-01-29 17:51:09 +0300

    Auth and Reg

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,50 +1,115 @@
 import { Button } from "@mui/material";
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
-import TextField from '@mui/material/TextField';
-
 
 export function AuthPage() {
   const navigate = useNavigate();
-  const click = () => { navigate('/');};
-  const clickreg = () => { navigate('/registration');};
-  const helppass = () => {};
+  const click = () => {
+    navigate("/");
+  };
+  const clickreg = () => {
+    navigate("/registration");
+  };
+
+  const helppass = () => {
+    toggleModal;
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    if (isModalOpen) {
+      console.log(isModalOpen);
+      setIsModalOpen(false);
+    } else {
+      console.log(isModalOpen);
+      setIsModalOpen(true);
+    }
+  };
+
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleChangep = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleChangee = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+  const clicktest = () => {
+    console.log(emailRegex.test(email));
+    console.log(passwordRegex.test(password)); // Здесь вы можете использовать значение пароля
+  };
 
   return (
     <>
-    <div className="back">
-      <div className="indiv">
-        <div className="Headbox">
-          <header className="HEAD">ВХОД В ПРОФИЛЬ</header>
-          <h5 className="Logintext">Логин</h5>
-          <div className="Loginbox"><TextField label="Enter your E-mail..." id="fullWidth" /></div>
-          <h5 className="Passwordtext">Пароль</h5>
-          <div className="Passwordbox"><TextField label="Enter your password..." id="fullWidth" /></div>
-          <Button onClick={helppass}
-                  sx={{
-                    color: "white",
-                    fontSize: 10,
-                    top: 15,
-                    left: 57,
-                    fontFamily: "Montserrat",
-                  }}
-                >
-                  Забыли пароль?
-          </Button>
-          <div className="Button"><button className="buttonAuth">Войти</button></div>
-          <div className="Button"><button className="buttonReg" onClick={clickreg}>Зарегистрироваться</button></div>
-          <Button onClick={click}
-                    color="white"
-                    sx={{
-                      color: "white",
-                      fontSize: 10,
-                      top: 50,
-                      left: 155,
-                    }}
-                  >Назад на главную страницу</Button>
+      <div className="back">
+        <div className="indiv">
+          <div className="Headbox">
+            <header className="HEAD">ВХОД В ПРОФИЛЬ</header>
+            <h5 className="Logintext">Логин</h5>
+            <div className="Loginbox">
+              <TextField
+                id="fullWidth"
+                type="email"
+                value={email}
+                onChange={handleChangee}
+                fullWidth
+              />
+            </div>
+            <h5 className="Passwordtext">Пароль</h5>
+            <div className="Passwordbox">
+              <TextField
+                id="fullWidth"
+                type="password"
+                value={password}
+                onChange={handleChangep}
+                fullWidth
+              />
+            </div>
+            <Button
+              onClick={toggleModal}
+              sx={{
+                color: "white",
+                fontSize: 10,
+                top: 15,
+                left: 57,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Забыли пароль?
+            </Button>
+            <div className="Button">
+              <button className="buttonAuth" onClick={clicktest}>
+                Войти
+              </button>
+            </div>
+            <div className="Button">
+              <button className="buttonReg" onClick={clickreg}>
+                Зарегистрироваться
+              </button>
+            </div>
+            <Button
+              onClick={click}
+              color="white"
+              sx={{
+                color: "white",
+                fontSize: 10,
+                top: 50,
+                left: 155,
+              }}
+            >
+              Назад на главную страницу
+            </Button>
+          </div>
         </div>
-      </div>  
-    </div>
+      </div>
     </>
   );
 }
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,41 +1,59 @@
 import { Button } from "@mui/material";
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 
 export function RegistrationPage() {
   const navigate = useNavigate();
-  const click = () => { navigate('/');};
-  const clickrev = () => { navigate('/auth');};
+  const click = () => {
+    navigate("/");
+  };
+  const clickrev = () => {
+    navigate("/auth");
+  };
 
   return (
     <>
-    <div className="back">
-      <div className="indiv">
-        <div className="Headbox">
-          <header className="HEAD">РЕГИСТАЦИЯ</header>
-          <h5 className="Logintext1">Логин</h5>
-          <div className="Loginbox1"><TextField label="Enter your Login..." id="fullWidth" /></div>
-          <h5 className="Etext">E-mail</h5>
-          <div className="Ebox"><TextField label="Enter your E-mail..." id="fullWidth" /></div>
-          <h5 className="Passwordtext1">Пароль</h5>
-          <div className="Passwordbox1"><TextField label="Enter your Password..." id="fullWidth" /></div>
-          <h5 className="Passwordtext11">Повтор пароля  </h5>
-          <div className="Passwordbox12"><TextField label="Enter your  repeat Password..." id="fullWidth" /></div>
-          <div className="Button"><button className="buttonrev" onClick={clickrev}>Зарегистрироваться</button></div>
-          <Button onClick={clickrev}
-                    color="white"
-                    sx={{
-                      color: "white",
-                      fontSize: 11,
-                      top: 40,
-                      left: 190,
-                    }}
-                  >Назад ко входу</Button>
+      <div className="back">
+        <div className="indiv">
+          <div className="Headbox">
+            <header className="HEAD">РЕГИСТАЦИЯ</header>
+            <h5 className="Logintext1">Логин</h5>
+            <div className="Loginbox1">
+              <TextField id="fullWidth" />
+            </div>
+            <h5 className="Etext">E-mail</h5>
+            <div className="Ebox">
+              <TextField id="fullWidth" />
+            </div>
+            <h5 className="Passwordtext1">Пароль</h5>
+            <div className="Passwordbox1">
+              <TextField id="fullWidth" />
+            </div>
+            <h5 className="Passwordtext11">Повтор пароля </h5>
+            <div className="Passwordbox12">
+              <TextField id="fullWidth" />
+            </div>
+            <div className="Button">
+              <button className="buttonrev" onClick={clickrev}>
+                Зарегистрироваться
+              </button>
+            </div>
+            <Button
+              onClick={clickrev}
+              color="white"
+              sx={{
+                color: "white",
+                fontSize: 11,
+                top: 40,
+                left: 190,
+              }}
+            >
+              Назад ко входу
+            </Button>
+          </div>
         </div>
-      </div>  
-    </div>
+      </div>
     </>
   );
 }
-
